01feb084fe32d9db28efa128437afe0f6d3ef23a
shaunbseidman <31448025+shaunbseidman@users.noreply.github.com>
2018-01-02 12:49:28 -0700

    servedata3

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 4000;
+app.use(cors())
+const data = require('./content.json')
+
+app.get('/', function(req,res) {
+  res.json({data})
+})
+
+function studentInfo(data, id){
+    for (let i = 0; i < data.length; i++){
+        if (data[i].id == id){
+            return data[i]
+        }
+    }
+    return null
+}
+
+app.get('/:id', function(req, res) {
+  var record = studentInfo(data, req.params.id)
+  if (!record) {
+    res.status(404).json({
+      error: {message: "No record found!"}
+    })
+  } else{
+    res.json({data: record})
+  }
+})
+
+
+
+app.listen(port, function(req,res) {
+  console.log('app is listening on ' + port);
+})
